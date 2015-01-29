22ab9c97fe20b3b97d5e4210bf0156a04c2afe7c
Andrew Jiang <andrew.h.jiang@gmail.com>
2015-01-29 09:36:32 -0800

    Fix signup and login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,3 +2,15 @@ class HomeController < ApplicationController
   def index
   end
 end
+
+def resource_name
+  :user
+end
+
+def resource
+  @resource ||= User.new
+end
+
+def devise_mapping
+  @devise_mapping ||= Devise.mappings[:user]
+end
