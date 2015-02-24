4fb72229e9da7b239b75289152320614da8d9177
Andrew Jiang <andrew.h.jiang@gmail.com>
2015-02-23 17:29:56 -0800

    Small website changes for mixpanel tracking and removing Typeform

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
 
+	$("#home-bottom-start-btn").click(function(e) {
+		$("#home-hero-email").focus();
+	});
+
+	$("#header-start-btn").click(function(e) {
+
+		if (window.location.pathname == "/"){
+			$("#home-hero-email").focus();
+		} else{
+			window.location.href = "/";
+			$("#home-hero-email").focus();
+		}
+	});
+
 	$("#home-hero-button").click(function(e) {
 
 		var email = $("#home-hero-email").val();
 
 		if (email){
 			window.location.href = "/?email=" + email;
+			mixpanel.track("Submitted Info");
 		} else {
 
 			$("#message").css("color","red")
 			$("#message").html("Please enter your email address");
+			mixpanel.track("Submitted Blank");
 		}
 		
 	    
